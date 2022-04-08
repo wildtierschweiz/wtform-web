a6f80f92d920688e868eea7f814115e3507d7ce6
Daniel Duersteler <daniel.duersteler@wildtier.ch>
2022-04-08 03:59:10 +0200

    streamlined code

--- src/Controller/data.php
+++ src/Controller/data.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace WildtierSchweiz\WtFormWeb\Controller;
 
+use Exception;
 use WildtierSchweiz\WtFormWeb\Application;
 use WildtierSchweiz\WtFormWeb\Service\FormService;
 
@@ -21,8 +22,12 @@ final class data extends Application
             self::$_f3->set('VIEWVARS.forms', $_form_service->getFormList($_form_lang));
             return;
         }
-        self::$_f3->set('RESPONSE.data', $_form_service->getFormDataCsv($_form_slug));
-        self::$_f3->set('RESPONSE.mime', 'text/csv');
-        self::$_f3->set('RESPONSE.filename', 'data-' . $_form_slug . '.csv');
+        try {
+            self::$_f3->set('RESPONSE.data', $_form_service->getFormDataCsv($_form_slug));
+            self::$_f3->set('RESPONSE.mime', 'text/csv');
+            self::$_f3->set('RESPONSE.filename', 'data-' . $_form_slug . '.csv');
+        } catch (Exception $e_) {
+            self::$_f3->error(500, $e_->getMessage());
+        }
     }
 }
--- src/Controller/forms.php
+++ src/Controller/forms.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace WildtierSchweiz\WtFormWeb\Controller;
 
+use Exception;
 use WildtierSchweiz\WtFormWeb\Application;
 use WildtierSchweiz\WtFormWeb\Service\FormService;
 
@@ -17,8 +18,16 @@ final class forms extends Application
         $_form_service = FormService::instance();
         $_form_slug = self::$_f3->get('PARAMS.form');
         $_form_lang = self::$_f3->get('PARAMS.lang');
-        $_form_service->loadForm($_form_slug, $_form_lang);
-        self::$_f3->set('VIEWVARS.form', $_form_service->getForm());
+        if (!$_form_slug) {
+            self::$_f3->error(404);
+            return;
+        }
+        try {
+            $_form_service->loadForm($_form_slug, $_form_lang);
+            self::$_f3->set('VIEWVARS.form', $_form_service->getForm());
+        } catch (Exception $e_) {
+            self::$_f3->error(500, $e_->getMessage());
+        }
     }
 
     /**
@@ -29,9 +38,13 @@ final class forms extends Application
         $_form_service = FormService::instance();
         $_form_slug = self::$_f3->get('PARAMS.form');
         $_form_lang = self::$_f3->get('PARAMS.lang');
-        $_form_service->loadForm($_form_slug, $_form_lang);
-        if ($_form_service->validateForm() === true)
-            $_form_service->postForm(self::$_f3->get('POST'));
-        self::$_f3->set('VIEWVARS.form', $_form_service->getForm());
+        try {
+            $_form_service->loadForm($_form_slug, $_form_lang);
+            if ($_form_service->validateForm() === true)
+                $_form_service->postForm(self::$_f3->get('POST'));
+            self::$_f3->set('VIEWVARS.form', $_form_service->getForm());
+        } catch (Exception $e_) {
+            self::$_f3->error(500, $e_->getMessage());
+        }
     }
 }
