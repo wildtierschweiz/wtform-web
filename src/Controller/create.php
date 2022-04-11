c4ecbfe1791f6d42e07a3e2dbfc0f991288ab581
Daniel Duersteler <daniel.duersteler@wildtier.ch>
2022-04-11 20:05:57 +0200

    working on create form

--- src/Controller/create.php
+++ src/Controller/create.php
@@ -4,15 +4,25 @@ declare(strict_types=1);
 
 namespace WildtierSchweiz\WtFormWeb\Controller;
 
+use Log;
 use WildtierSchweiz\WtFormWeb\Application;
-use WildtierSchweiz\WtFormWeb\Model\Form;
 
 final class create extends Application
 {
     function get()
     {
-        $_form = new Form();
+        foreach (glob('../dict/*.ini') as $_file) {
 
-        self::$_f3->set('VIEWVARS.formlist', $_form->getForms());
+            $_lang = explode('.', end(explode('/', $_file)))[0];
+            //$_language = end
+
+            $_log = new Log('log.txt');
+            $_log->write($_lang);
+
+            self::$_f3->push('VIEWVARS.langlist', [
+                'key' => $_lang,
+                'value' => strtoupper($_lang)
+            ]);
+        }
     }
 }
