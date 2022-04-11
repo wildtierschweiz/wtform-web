2132678fefe1286f8432f783ac1983968276f5de
Daniel Duersteler <daniel.duersteler@wildtier.ch>
2022-04-11 20:18:50 +0200

    working on create form

--- src/Controller/create.php
+++ src/Controller/create.php
@@ -12,13 +12,7 @@ final class create extends Application
     function get()
     {
         foreach (glob('../dict/*.ini') as $_file) {
-
             $_lang = explode('.', end(explode('/', $_file)))[0];
-            //$_language = end
-
-            $_log = new Log('log.txt');
-            $_log->write($_lang);
-
             self::$_f3->push('VIEWVARS.langlist', [
                 'key' => $_lang,
                 'value' => strtoupper($_lang)
---
+++ src/Service/FormCreatorService.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WildtierSchweiz\WtFormWeb\Service;
+
+use Basket;
+use Prefab;
+
+/**
+ * create form session based
+ */
+class FormCreatorService extends Prefab
+{
+    /**
+     * @var array
+     * form configuration
+     */
+    static private Basket $_form;
+
+    function __construct(string $name_ = 'form')
+    {
+        self::$_form = new Basket($name_);
+    }
+
+    
+}
